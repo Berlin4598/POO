6250ccc573e2e3b39a4810267186f0bc15f5010b
Cesar López <cesar.plyed@gmail.com>
2025-09-18 12:11:58 -0600

    s

--- IMC/obj/Debug/net8.0/IMC.AssemblyInfo.cs
+++ IMC/obj/Debug/net8.0/IMC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d2ec80e304ea9c853b25faddc5b3e11a3c921d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c481509d08cb6e4c7ba73190f49251d20828f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
